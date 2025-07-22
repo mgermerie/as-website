6ca0f87c52733668e2f7b78a61ac63948922f695
mgermerie <73115044+mgermerie@users.noreply.github.com>
2025-07-22 17:11:08 +0200

    fix team join on personal results

--- controllers/personalResults.php
+++ controllers/personalResults.php
@@ -6,11 +6,23 @@ $pageTitle = 'Mes résultats';
 
 # MODELS
 
+require_once( './models/DatabaseManager.php' );
+require_once( './models/TeamsManager.php' );
 require_once( './models/navigation.php' );
 
 
 $navData = get_nav_items();
 
+
+$database = new DatabaseManager();
+
+if ( isset( $_SESSION['LOGGED_USER']['needsTeam'] ) )
+{
+    unset( $_SESSION['LOGGED_USER']['needsTeam'] );
+    $teamsManager = new TeamsManager( $database );
+    $teamsList = $teamsManager->get_teams();
+    $needsTeam = true;
+}
 
 # VIEWS
 
